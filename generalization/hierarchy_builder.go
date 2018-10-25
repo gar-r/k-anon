9a83c913caeea0d86eb72f7ca6b50c27f9798e55
dargzero <dargzero@gmail.com>
2018-10-25 23:17:29 +0200

    int hierarchy builder

--- generalization/hierarchy_builder.go
+++ generalization/hierarchy_builder.go
@@ -11,9 +11,58 @@ type IntegerHierarchyBuilder struct {
 
 func (b *IntegerHierarchyBuilder) Build() *Hierarchy {
 	b.deduplicate()
-	b.sort()
-	l := []*Partition{NewPartition(b.Items)}
-	return &Hierarchy{}
+	h := &Hierarchy{
+		Partitions: [][]*Partition{
+			{NewPartition(stripType(b.Items)...)},
+		},
+	}
+	for !refined(h) {
+		refine(h)
+	}
+	return h
+}
+
+func refine(h *Hierarchy) {
+	level := h.getLevel(0)
+	var newPartitions []*Partition
+	for _, p := range level {
+		if len(p.items) > 1 {
+			values := intValues(p)
+			p1, p2 := split(values)
+			newPartitions = append(newPartitions, NewPartition(stripType(p1)...))
+			newPartitions = append(newPartitions, NewPartition(stripType(p2)...))
+		} else {
+			newPartitions = append(newPartitions, p)
+		}
+	}
+	h.Partitions = append([][]*Partition{newPartitions}, h.Partitions...)
+}
+
+func refined(h *Hierarchy) bool {
+	level := h.getLevel(0)
+	for _, p := range level {
+		if len(p.items) != 1 {
+			return false
+		}
+	}
+	return true
+}
+
+func intValues(p *Partition) []int {
+	var values []int
+	for item := range p.items {
+		i := item.(int)
+		values = append(values, i)
+	}
+	return values
+}
+
+func stripType(slice []int) []interface{} {
+	var result []interface{}
+	for _, item := range slice {
+		result = append(result, item)
+	}
+	return result
 }
 
 func (b *IntegerHierarchyBuilder) deduplicate() {
@@ -27,16 +76,25 @@ func (b *IntegerHierarchyBuilder) deduplicate() {
 	}
 }
 
-func (b *IntegerHierarchyBuilder) sort() {
-	sort.Ints(b.Items)
-}
-
-func split(slice []int) (p1, p2 []int) {
-
-}
-
-func median(slice []int) int {
-	if len(slice)%2 == 1 {
-
+func split(slice []int) ([]int, []int) {
+	m := median(slice)
+	var p1 []int
+	var p2 []int
+	for _, i := range slice {
+		if float64(i) < m {
+			p1 = append(p1, i)
+		} else {
+			p2 = append(p2, i)
+		}
 	}
+	return p1, p2
+}
+
+func median(slice []int) float64 {
+	len := len(slice)
+	sort.Ints(slice)
+	if len%2 == 1 {
+		return float64(slice[len/2])
+	}
+	return float64(slice[len/2-1]+slice[len/2]) / 2
 }
--- generalization/hierarchy_builder_test.go
+++ generalization/hierarchy_builder_test.go
@@ -1,8 +1,12 @@
 package generalization
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_EmptyBuilder(t *testing.T) {
-	builder := &IntegerHierarchyBuilder{}
-	builder.Build()
+	builder := &IntegerHierarchyBuilder{
+		Items: []int{2, 4, 6, 8, 1, 3, 5, 7, 9},
+	}
+	builder.Build() // TODO: tests
 }
