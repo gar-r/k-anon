200820c75c0b3b643d1d945b4beb853c79667892
Richard <dargzero@gmail.com>
2018-10-25 13:46:49 +0000

    test invalid hierarchy in generalizer

--- generalization/generalizer_test.go
+++ generalization/generalizer_test.go
@@ -4,6 +4,24 @@ import (
 	"testing"
 )
 
+func Test_InvalidHierarchy(t *testing.T) {
+	invalid := &Hierarchy{
+		Partitions: [][]*Partition{
+			{
+				NewPartition("A"),
+				NewPartition("B"),
+			},
+			{
+				NewPartition("C"),
+			},
+		},
+	}
+	generalizer := NewHierarchyGeneralizer(invalid)
+	if nil != generalizer {
+		t.Errorf("Expected nil, but got %v", generalizer)
+	}
+}
+
 func Test_HierarchyGeneralizer_Level1(t *testing.T) {
 	generalizer := NewHierarchyGeneralizer(getExampleHierarchy())
 	actual := generalizer.Generalize("C", 1)
