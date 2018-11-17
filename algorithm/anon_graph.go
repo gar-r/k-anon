38f3191d22ca3c08fbabecb84d4096e074eba19d
dargzero <dargzero@gmail.com>
2018-11-17 11:05:43 +0100

    checkpoint

--- algorithm/anon_graph.go
+++ algorithm/anon_graph.go
@@ -1,7 +1,25 @@
 package algorithm
 
-//func BuildAnonGraph(table *model.Table, k int) goraph.Graph {
-//	costGraph := BuildCostGraph(table)
-//	anonGraph := BuildCoreGraph(table)
-//	anonGraph.GetTargets()
-//}
+import (
+	"gonum.org/v1/gonum/graph"
+	"gonum.org/v1/gonum/graph/simple"
+	"gonum.org/v1/gonum/graph/topo"
+)
+
+func BuildAnonGraph(costGraph graph.WeightedUndirected, k int) graph.Directed {
+	g := simple.NewDirectedGraph()
+	for !isComplete(g, k) {
+
+	}
+	return g
+}
+
+func isComplete(g graph.Directed, k int) bool {
+	components := topo.ConnectedComponents(graph.Undirect{G: g})
+	for _, c := range components {
+		if len(c) < k {
+			return false
+		}
+	}
+	return true
+}
